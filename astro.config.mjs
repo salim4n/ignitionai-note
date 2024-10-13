6c2ad122978710ea05a5f333ba9445d00a2ae6f8
salim laimeche <slaimeche@webexpr.fr>
2024-10-13 18:10:05 +0200

    Removed rehypeKatex integration from markdown configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,11 @@
 import { defineConfig } from "astro/config"
 import starlight from "@astrojs/starlight"
 import remarkMath from "remark-math"
-import rehypeKatex from "rehype-katex"
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [
-      [
-        rehypeKatex,
-        {
-          // Katex plugin options
-        },
-      ],
-    ],
   },
   integrations: [
     starlight({
